6e03ddf6c434aec011f05cbd91b86f162a4e7699
NamanAryan <namanaryan08@gmail.com>
2025-02-17 17:45:01 +0530

    fix: update import path for authentication middleware in order route

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,6 +1,6 @@
 import Order from '../models/order.model.js';
 import { Router } from 'express';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/auth.middleware.js';
 const router = Router();
 
 router.post('/create', protect, async (req, res) => {
